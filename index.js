5b739f90625cadeddc71efe4d773286deda3fbca
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2022-08-12 21:32:27 +0200

    async function

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ import ('dotenv/config');
 const baseApiUrl = 'https://fapi.apollox.finance/fapi/v1';
 const baseWssUrl = 'wss://fstream.apollox.finance/ws';
 
-const key = process.env.PANCAKE_KEY;
-const secret = process.env.PANCAKE_SECRET;
+const key = process.env.PANCAKE_KEY || (() => { throw "access key required"; })();
+const secret = process.env.PANCAKE_SECRET || (() => { throw "secret key required"; })();
+
+let executions = {};
 
 async function signedPost(action, queryObj) {
     queryObj = {
@@ -49,6 +51,37 @@ async function callFetch(action, method) {
     return responseJson;
 }
 
+function keygen(message) {
+    if (!message || !message.e) return null;
+    if (message.e == 'ORDER_TRADE_UPDATE') return `${message.e}:${message.o.i}:${message.o.X}`;
+    return null;
+}
+
+const symbol = process.env.PANCAKE_SYMBOL || (() => { throw "symbol required"; })();
+const strikePrice = parseFloat(process.env.PANCAKE_STRIKE_PRICE) || (() => { throw "strike price needs to be a number"; })();
+const closePercent = parseFloat(process.env.PANCAKE_CLOSE_PERCENT) || (() => { throw "close percent needs to be a number"; })();
+
+const closePrice = strikePrice * (1 + closePercent);
+
+async function closeFilled(message) {
+    const shortResponse = await signedPost('order', {
+
+    });
+
+    executions[""] = shortFilled;
+    return { removeKey: true };
+}
+
+async function shortFilled(message) {
+    const shortResponse = await signedPost('order', {
+
+    });
+
+    executions[""] = shortFilled;
+
+
+    return { removeKey: true };
+}
 
 try {
     var { listenKey } = await callFetch('listenKey');
@@ -59,19 +92,22 @@ try {
 
     var socket = new WebSocket(`${baseWssUrl}/${listenKey}`);
 
-    socket.on('message', function message(data) {
-        try {
-            const message = JSON.parse(data.toString('utf-8'));
-            const execKey = keygen(message);
-            if (!execKey) return;
+    socket.on('message', data => {
+        (async() => {
+            try {
+                const message = JSON.parse(data.toString('utf-8'));
+                const execKey = keygen(message);
+                if (!execKey) return;
 
-            execFunc = executions[execKey];
-            if (!execFunc) return;
+                const execFunc = executions[execKey];
+                if (!execFunc) return;
 
-            execFunc(message);
-        } catch (ex) {
-            console.error(ex);
-        }
+                const result = await execFunc(message);
+                if (result.removeKey) delete executions[execKey];
+            } catch (ex) {
+                console.error(ex);
+            }
+        })();
     });
 
     process.stdin.setRawMode(true);
