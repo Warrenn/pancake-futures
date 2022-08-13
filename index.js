7ee8f3eab92c77556a7a7e1c582a7e094c6e7a70
Warrenn <warrenn@busyweb.co.za>
2022-08-13 13:41:00 +0200

    wont work first time

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
 const { exit } = await
-import ('node:process');
+import('node:process');
 const { WebSocket } = await
-import ('ws');
+import('ws');
 const { createHmac } = await
-import ('node:crypto');
+import('node:crypto');
 const querystring = await
-import ('node:querystring');
+import('node:querystring');
 await
-import ('dotenv/config');
+import('dotenv/config');
+const { setTimeout: asyncSleep } = await import('timers/promises');
 
 const baseApiUrl = 'https://fapi.apollox.finance/fapi/v1';
 const baseWssUrl = 'wss://fstream.apollox.finance/ws';
+const recvWindow = 99999;
 
 const key = process.env.PANCAKE_KEY || (() => { throw "access key required"; })();
 const secret = process.env.PANCAKE_SECRET || (() => { throw "secret key required"; })();
 
-let executions = {};
+const symbol = process.env.PANCAKE_SYMBOL || (() => { throw "symbol required"; })();
+const strikePrice = parseFloat(process.env.PANCAKE_STRIKE_PRICE) || (() => { throw "strike price needs to be a number"; })();
+const closePercent = parseFloat(process.env.PANCAKE_CLOSE_PERCENT) || (() => { throw "close percent needs to be a number"; })();
+const cashSize = parseFloat(process.env.PANCAKE_CASH_SIZE) || (() => { throw "cash size needs to be a number"; })();
+const sleepInterval = parseInt(process.env.PANCAKE_SLEEP) || 3000;
 
-async function signedPost(action, queryObj) {
+let executions = {};
+let intervalRef = null;
+
+async function signedFetch(action, queryObj, method) {
     queryObj = {
-        "recvWindow": 99999,
+        "recvWindow": recvWindow,
         ...queryObj,
         "timestamp": Date.now()
     };
 
+    method = method || 'POST';
     const query = querystring.encode(queryObj);
     const hash = createHmac('sha256', secret)
         .update(query)
         .digest('hex');
 
     const response = await fetch(`${baseApiUrl}/${action}?${query}&signature=${hash}`, {
-        method: 'POST',
+        method: method,
         headers: {
             "X-MBX-APIKEY": key
         }
@@ -53,32 +63,86 @@ async function callFetch(action, method) {
 
 function keygen(message) {
     if (!message || !message.e) return null;
-    if (message.e == 'ORDER_TRADE_UPDATE') return `${message.e}:${message.o.i}:${message.o.X}`;
+    if (message.e == 'ORDER_TRADE_UPDATE') return `ORDER_TRADE_UPDATE:${message.o.i}:${message.o.X}`;
     return null;
 }
 
-const symbol = process.env.PANCAKE_SYMBOL || (() => { throw "symbol required"; })();
-const strikePrice = parseFloat(process.env.PANCAKE_STRIKE_PRICE) || (() => { throw "strike price needs to be a number"; })();
-const closePercent = parseFloat(process.env.PANCAKE_CLOSE_PERCENT) || (() => { throw "close percent needs to be a number"; })();
+const placeClose = async (o) => await signedFetch('order', {
+    "symbol": symbol,
+    "side": "BUY",
+    "type": "STOP",
+    "price": o.price,
+    "stopPrice": o.price,
+    "closePosition": true
+});
 
-const closePrice = strikePrice * (1 + closePercent);
+const placeShort = async (o) => await signedFetch('order', {
+    "symbol": symbol,
+    "side": "SELL",
+    "type": "STOP",
+    "price": o.price,
+    "stopPrice": o.price,
+    "size": o.size
+});
 
 async function closeFilled(message) {
-    const shortResponse = await signedPost('order', {
+    const lastPrice = parseFloat(message.o.L);
+    const price = lastPrice * (1 - closePercent);
+    const size = cashSize / price;
 
-    });
+    if (intervalRef) {
+        clearInterval(intervalRef);
+        intervalRef = null;
+    }
 
-    executions[""] = shortFilled;
+    const orderResponse = await placeShort({ price, size });
+    const callbackKey = `ORDER_TRADE_UPDATE:${orderResponse.orderId}:FILLED`;
+
+    executions[callbackKey] = shortFilled;
+
+    intervalRef = setInterval(async () => {
+        const marketResp = await callFetch('premiumIndex', 'GET');
+        if (marketResp.markPrice < strikePrice) return;
+        const strikeSize = cashSize / strikePrice;
+
+        await signedFetch('allOpenOrders', { symbol }, 'DELETE');
+        const strikeResponse = await placeShort({ price: strikePrice, size: strikeSize });
+
+        delete executions[callbackKey];
+        executions[`ORDER_TRADE_UPDATE:${strikeResponse.orderId}:FILLED`] = shortFilled;
+
+        clearInterval(intervalRef);
+    }, sleepInterval);
+
     return { removeKey: true };
 }
 
 async function shortFilled(message) {
-    const shortResponse = await signedPost('order', {
+    const lastPrice = parseFloat(message.o.L);
+    const price = lastPrice * (1 + closePercent);
 
-    });
+    if (intervalRef) {
+        clearInterval(intervalRef);
+        intervalRef = null;
+    }
 
-    executions[""] = shortFilled;
+    const orderResponse = await placeClose({ price });
+    const callbackKey = `ORDER_TRADE_UPDATE:${orderResponse.orderId}:FILLED`;
 
+    executions[callbackKey] = closeFilled;
+
+    intervalRef = setInterval(async () => {
+        const marketResp = await callFetch('premiumIndex', 'GET');
+        if (marketResp.markPrice > strikePrice) return;
+
+        await signedFetch('allOpenOrders', { symbol }, 'DELETE');
+        const strikeResponse = await placeClose({ price: strikePrice });
+
+        delete executions[callbackKey];
+        executions[`ORDER_TRADE_UPDATE:${strikeResponse.orderId}:FILLED`] = closeFilled;
+        clearInterval(intervalRef);
+
+    }, sleepInterval);
 
     return { removeKey: true };
 }
@@ -86,28 +150,26 @@ async function shortFilled(message) {
 try {
     var { listenKey } = await callFetch('listenKey');
 
-    var interval = setInterval(async() => {
+    var interval = setInterval(async () => {
         await callFetch('listenKey', 'PUT');
-    }, 59 * 60 * 1000 /*59 minutes: minutes * seconds * milliseconds*/ );
+    }, 59 * 60 * 1000 /*59 minutes: minutes * seconds * milliseconds*/);
 
     var socket = new WebSocket(`${baseWssUrl}/${listenKey}`);
 
-    socket.on('message', data => {
-        (async() => {
-            try {
-                const message = JSON.parse(data.toString('utf-8'));
-                const execKey = keygen(message);
-                if (!execKey) return;
+    socket.on('message', async data => {
+        try {
+            const message = JSON.parse(data.toString('utf-8'));
+            const execKey = keygen(message);
+            if (!execKey) return;
 
-                const execFunc = executions[execKey];
-                if (!execFunc) return;
+            const execFunc = executions[execKey];
+            if (!execFunc) return;
 
-                const result = await execFunc(message);
-                if (result.removeKey) delete executions[execKey];
-            } catch (ex) {
-                console.error(ex);
-            }
-        })();
+            const result = await execFunc(message);
+            if (result.removeKey) delete executions[execKey];
+        } catch (ex) {
+            console.error(ex);
+        }
     });
 
     process.stdin.setRawMode(true);
