deca3e19b65f823927d602e8a37ba5414450a318
warrenn <warrenn@busyweb.co.za>
2022-08-15 17:51:04 +0200

    fixing issues

--- index.js
+++ index.js
@@ -19,8 +19,6 @@ const secret = process.env.PANCAKE_SECRET || (() => { throw "secret key required
 
 const symbol = process.env.PANCAKE_SYMBOL || (() => { throw "symbol required"; })();
 const closePercent = parseFloat(process.env.PANCAKE_CLOSE_PERCENT) || (() => { throw "close percent needs to be a number"; })();
-const callbackRate = parseFloat(process.env.PANCAKE_CALLBACK_RATE) || (() => { throw "callback rate needs to be a number"; })();
-const inverval = parseInt(process.env.PANCAKE_INTERVAL) || 1000;
 
 const cashSize = parseFloat(process.env.PANCAKE_CASH_SIZE) || (() => { throw "cash size needs to be a number"; })();
 const strikePrice = parseFloat(process.env.PANCAKE_STRIKE_PRICE) || (() => { throw "strike price needs to be a number"; })();
@@ -50,6 +48,7 @@ async function signedFetch(action, queryObj, method) {
         const responseJson = await response.json();
         return responseJson;
     } catch (e) {
+        console.error(`signedFetch error ${baseApiUrl}/${action}?${query}&signature=${hash}`);
         console.error(e);
         return null;
     }
@@ -70,6 +69,7 @@ async function callFetch(action, queryObj, method) {
         return responseJson;
     }
     catch (e) {
+        console.error(`callFetch error ${baseApiUrl}/${action}${query}`);
         console.error(e);
         return null;
     }
@@ -100,24 +100,6 @@ const placeShort = async (o) => await signedFetch('order', {
     "quantity": o.quantity
 });
 
-const placeTriggerReduce = async (o) => await signedFetch('order', {
-    "symbol": symbol,
-    "side": "BUY",
-    "type": "TRAILING_STOP_MARKET",
-    "quantity": o.quantity,
-    "reduceOnly": true,
-    "activationPrice": o.activationPrice,
-    "callbackRate": o.callbackRate
-});
-
-const placeTriggerShort = async (o) => await signedFetch('order', {
-    "symbol": symbol,
-    "side": "SELL",
-    "type": "TRAILING_STOP_MARKET",
-    "quantity": o.quantity,
-    "activationPrice": o.activationPrice,
-    "callbackRate": o.callbackRate
-});
 
 const round1 = num => Math.round(num * 10) / 10;
 const round3 = num => Math.round(num * 1000) / 1000;
@@ -143,38 +125,58 @@ async function shortFilled(message) {
     return { removeKey: true };
 }
 
+async function onMessage(data) {
+    try {
+        const dataText = data.toString('utf-8');
+        const message = JSON.parse(dataText);
+        console.log(message.e);
+        const execKey = keygen(message);
+        if (!execKey) return;
+
+        const execFunc = executions[execKey];
+        if (!execFunc) return;
+
+        const result = await execFunc(message);
+        if (result.removeKey) delete executions[execKey];
+    } catch (ex) {
+        console.error('message error');
+        console.error(ex);
+    }
+}
+
 try {
+    console.log(`strike-price ${strikePrice} close-price ${closePrice}`);
     var { listenKey } = await callFetch('listenKey');
     if (!listenKey) process.exit();
 
-    var listenRef = setInterval(async () => {
-        await callFetch('listenKey', null, 'PUT');
-    }, 59 * 60 * 1000 /*59 minutes: minutes * seconds * milliseconds*/);
-
     var socket = new WebSocket(`${baseWssUrl}/${listenKey}`);
+    socket.on('message', async data => await onMessage(data));
 
-    socket.on('message', async data => {
-        try {
-            const message = JSON.parse(data.toString('utf-8'));
-            const execKey = keygen(message);
-            if (!execKey) return;
+    var listenRef = setInterval(async () => {
+        console.log('renewing key');
+        var putResponse = await callFetch('listenKey', 'PUT');
+        if (putResponse) return;
 
-            const execFunc = executions[execKey];
-            if (!execFunc) return;
-
-            const result = await execFunc(message);
-            if (result.removeKey) delete executions[execKey];
-        } catch (ex) {
-            console.error(ex);
+        console.error(`key:${listenKey} not found`);
+        if (socket && socket.OPEN) socket.close();
+        await callFetch('listenKey', 'DELETE');
+        
+        while (true) {
+            let listResponse = await callFetch('listenKey');
+            if (!listResponse || !listResponse.listenKey) continue;
+            listenKey = listResponse.listenKey;
+            break;
         }
-    });
+        
+        socket = new WebSocket(`${baseWssUrl}/${listenKey}`);
+        socket.on('message', async data => await onMessage(data));
+
+    }, 30000/*59 minutes: 59 minutes * 60 seconds * 1000 milliseconds*/);
 
     await closeFilled({ o: { L: strikePrice } });
 
     process.stdin.resume();
     process.stdin.on('data', process.exit.bind(process, 0));
-
-    clearInterval(listenRef);
 } catch (ex) {
     console.error(ex);
 }
