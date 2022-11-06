1a72cccac67b328a5ab5b39f67bc8f0ea458a995
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2022-11-06 15:31:58 +0200

    strategy

--- by-bit/out/index.js
+++ by-bit/out/index.js
@@ -1,33 +1,149 @@
 var _a;
-import { SpotClientV3 } from "bybit-api";
+import { setTimeout as asyncSleep } from 'timers/promises';
+import { SpotClientV3, WebsocketClient } from "bybit-api";
 import dotenv from "dotenv";
+import { exit } from "process";
 dotenv.config();
+const orderPrecision = 2;
 const symbol = 'ETHUSDT';
-const strikePrice = 365;
+const coin = 'ETH';
+const strikePrice = 363;
 const quantity = 6;
-let client = new SpotClientV3({
-    testnet: !!((_a = process.env.TESTNET) === null || _a === void 0 ? void 0 : _a.localeCompare("false", 'en', { sensitivity: 'accent' })),
+const useTestnet = !!((_a = process.env.TESTNET) === null || _a === void 0 ? void 0 : _a.localeCompare("false", 'en', { sensitivity: 'accent' }));
+let inprocess = false;
+const strikeLower = strikePrice * 0.9962191781; //slippage
+const strikeUpper = strikePrice * 1.006931507; //slippage
+const client = new SpotClientV3({
+    testnet: useTestnet,
     key: process.env.API_KEY,
     secret: process.env.API_SECRET
 });
-//first get the current position
-//if we are holding a short and long nothing to do
-//if we are holding a short only check price if we need to do a long
-//if we are not holding any position initiate the short order with condition
-while (true) {
-    let xaci = await client.getCrossMarginAccountInfo();
+const wsClient = new WebsocketClient({
+    testnet: useTestnet,
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET,
+    market: 'spotv3'
+});
+async function conditionalSell(orderQty, triggerPrice) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    triggerPrice = parseFloat(triggerPrice.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Sell",
+            symbol: symbol,
+            triggerPrice: `${triggerPrice}`,
+            orderCategory: 1
+        });
+        console.log(`conditional sell: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0)
+            return;
+    }
+}
+async function immediateSell(orderQty) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Sell",
+            symbol: symbol
+        });
+        console.log(`immediate sell: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0)
+            return;
+    }
+}
+async function immediateBuy(orderQty) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Buy",
+            symbol: symbol
+        });
+        console.log(`immediate buy: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0)
+            return;
+    }
+}
+async function conditionalBuy(orderQty, triggerPrice) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    triggerPrice = parseFloat(triggerPrice.toFixed(4));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Buy",
+            symbol: symbol,
+            triggerPrice: `${triggerPrice}`,
+            orderCategory: 1
+        });
+        //TODO: figure out what to do if the order fails
+        console.log(`conditional buy: ${JSON.stringify(orderResponse, null, 2)}`);
+        if (orderResponse.retCode == 0)
+            return;
+    }
+}
+async function InitializePosition() {
+    if (inprocess)
+        return;
+    inprocess = true;
+    let { result: { loanAccountList } } = await client.getCrossMarginAccountInfo();
+    let position = loanAccountList.find(loanItem => loanItem.tokenId == coin) || { free: 0, loan: 0 };
+    let { result: { list: orders } } = await client.getOpenOrders(symbol, undefined, undefined, 1);
+    let hasPendingSell = !!orders.find(order => order.side == "SELL" && order.orderQty == `${quantity}`);
+    let hasPendingBuy = !!orders.find(order => order.side == "BUY" && order.orderQty == `${quantity}`);
+    let borrowing = position.loan >= quantity;
+    let holding = position.free >= quantity || hasPendingSell;
     let { result: { price } } = await client.getLastTradedPrice(symbol);
-    let rsp = await client.borrowCrossMarginLoan('ETH', `${quantity}`);
-    //if (price < strikePrice) {
-    let orderResponse = await client.submitOrder({
-        orderType: "MARKET",
-        orderQty: `${quantity}`,
-        side: "Sell",
-        symbol: symbol,
-        // triggerPrice: `${strikePrice}`,
-        // orderCategory: 1
-    }); //place the short order immediately
-    let rr = await client.repayCrossMarginLoan('ETH', `${quantity}`);
-    console.log(orderResponse);
+    while (price < strikeUpper && price > strikeLower) {
+        console.log('at strike waiting before continuing');
+        await asyncSleep(1000);
+        let { result: { price: updatedPrice } } = await client.getLastTradedPrice(symbol);
+        price = parseFloat(updatedPrice);
+    }
+    let aboveStrike = price > strikeUpper;
+    console.log(`borrowing: ${borrowing} aboveStrike: ${aboveStrike} holding: ${holding} sell: ${hasPendingSell} buy: ${hasPendingBuy}`);
+    if (!borrowing) {
+        let borrowResponse = await client.borrowCrossMarginLoan(coin, `${quantity}`);
+        //TODO: need to consider this properly
+        if (borrowResponse.retCode != 0)
+            exit(-1);
+    }
+    if (!borrowing && aboveStrike && !hasPendingSell)
+        await conditionalSell(quantity, strikeUpper);
+    if (!borrowing && !aboveStrike)
+        await immediateSell(quantity);
+    if (borrowing && aboveStrike && !holding)
+        await immediateBuy(quantity * strikeLower);
+    if (borrowing && aboveStrike && holding && !hasPendingSell)
+        await conditionalSell(quantity, strikeUpper);
+    if (borrowing && !aboveStrike && holding && !hasPendingSell)
+        await immediateSell(quantity);
+    if (borrowing && !aboveStrike && !holding && !hasPendingBuy)
+        await conditionalBuy(quantity * strikeLower, strikeLower);
+    inprocess = false;
+}
+try {
+    process.stdin.on('data', process.exit.bind(process, 0));
+    wsClient.on('update', data => {
+        console.log(`update: ${JSON.stringify(data, null, 2)}`);
+        (async () => { await InitializePosition(); })();
+    });
+    wsClient.subscribe(['ticketInfo', 'order'], true);
+    await InitializePosition();
+    while (true) {
+        await asyncSleep(1000);
+        // let rr = await client.repayCrossMarginLoan('ETH', `${quantity}`);
+    }
+}
+catch (err) {
+    console.error(err);
 }
 //# sourceMappingURL=index.js.map
--- by-bit/src/index.ts
+++ by-bit/src/index.ts
@@ -1,38 +1,165 @@
-import { SpotClientV3, SpotCrossMarginBorrowingInfoRequest } from "bybit-api";
+import { float } from "aws-sdk/clients/lightsail";
+import { setTimeout as asyncSleep } from 'timers/promises';
+import { SpotClientV3, WebsocketClient } from "bybit-api";
 import dotenv from "dotenv";
+import { exit } from "process";
 
 dotenv.config();
 
+const orderPrecision = 2;
 const symbol = 'ETHUSDT';
-const strikePrice = 365;
+const coin = 'ETH';
+const strikePrice = 363;
 const quantity = 6;
+const useTestnet = !!(process.env.TESTNET?.localeCompare("false", 'en', { sensitivity: 'accent' }));
+let inprocess = false;
 
-let client = new SpotClientV3({
-    testnet: !!(process.env.TESTNET?.localeCompare("false", 'en', { sensitivity: 'accent' })),
+const strikeLower = strikePrice * 0.9962191781;//slippage
+const strikeUpper = strikePrice * 1.006931507;//slippage
+
+const client = new SpotClientV3({
+    testnet: useTestnet,
     key: process.env.API_KEY,
     secret: process.env.API_SECRET
 });
 
-//first get the current position
-//if we are holding a short and long nothing to do
-//if we are holding a short only check price if we need to do a long
-//if we are not holding any position initiate the short order with condition
+const wsClient = new WebsocketClient({
+    testnet: useTestnet,
+    key: process.env.API_KEY,
+    secret: process.env.API_SECRET,
+    market: 'spotv3'
+});
 
+async function conditionalSell(orderQty: float, triggerPrice: float) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    triggerPrice = parseFloat(triggerPrice.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Sell",
+            symbol: symbol,
+            triggerPrice: `${triggerPrice}`,
+            orderCategory: 1
+        });
+        console.log(`conditional sell: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0) return;
+    }
+}
 
-while (true) {
-    let xaci = await client.getCrossMarginAccountInfo();
+async function immediateSell(orderQty: float) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Sell",
+            symbol: symbol
+        });
+        console.log(`immediate sell: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0) return;
+    }
+}
+
+async function immediateBuy(orderQty: float) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Buy",
+            symbol: symbol
+        });
+        console.log(`immediate buy: ${JSON.stringify(orderResponse, null, 2)}`);
+        //TODO: figure out what to do if the order fails
+        if (orderResponse.retCode == 0) return;
+    }
+}
+
+async function conditionalBuy(orderQty: float, triggerPrice: float) {
+    orderQty = parseFloat(orderQty.toFixed(orderPrecision));
+    triggerPrice = parseFloat(triggerPrice.toFixed(4));
+    while (true) {
+        let orderResponse = await client.submitOrder({
+            orderType: "MARKET",
+            orderQty: `${orderQty}`,
+            side: "Buy",
+            symbol: symbol,
+            triggerPrice: `${triggerPrice}`,
+            orderCategory: 1
+        });
+        //TODO: figure out what to do if the order fails
+        console.log(`conditional buy: ${JSON.stringify(orderResponse, null, 2)}`);
+        if (orderResponse.retCode == 0) return;
+    }
+}
+
+async function InitializePosition() {
+    if (inprocess) return;
+    inprocess = true;
+    let { result: { loanAccountList } } = await client.getCrossMarginAccountInfo();
+    let position = (<any[]>loanAccountList).find(loanItem => loanItem.tokenId == coin) || { free: 0, loan: 0 };
+
+    let { result: { list: orders } } = await client.getOpenOrders(symbol, undefined, undefined, 1);
+    let hasPendingSell = !!(<any[]>orders).find(order => order.side == "SELL" && order.orderQty == `${quantity}`);
+    let hasPendingBuy = !!(<any[]>orders).find(order => order.side == "BUY" && order.orderQty == `${quantity}`);
+
+    let borrowing = position.loan >= quantity;
+    let holding = position.free >= quantity || hasPendingSell;
     let { result: { price } } = await client.getLastTradedPrice(symbol);
-    let rsp = await client.borrowCrossMarginLoan('ETH', `${quantity}`);
-    //if (price < strikePrice) {
-    let orderResponse = await client.submitOrder({
-        orderType: "MARKET",
-        orderQty: `${quantity}`,
-        side: "Sell",
-        symbol: symbol,
-        // triggerPrice: `${strikePrice}`,
-        // orderCategory: 1
-    });        //place the short order immediately
 
-    let rr = await client.repayCrossMarginLoan('ETH', `${quantity}`);
-    console.log(orderResponse);
+    while (price < strikeUpper && price > strikeLower) {
+        console.log('at strike waiting before continuing');
+        await asyncSleep(1000);
+        let { result: { price: updatedPrice } } = await client.getLastTradedPrice(symbol);
+        price = parseFloat(updatedPrice);
+    }
+
+    let aboveStrike = price > strikeUpper;
+
+    console.log(`borrowing: ${borrowing} aboveStrike: ${aboveStrike} holding: ${holding} sell: ${hasPendingSell} buy: ${hasPendingBuy}`);
+
+    if (!borrowing) {
+        let borrowResponse = await client.borrowCrossMarginLoan(coin, `${quantity}`);
+        //TODO: need to consider this properly
+        if (borrowResponse.retCode != 0) exit(-1);
+    }
+
+    if (!borrowing && aboveStrike && !hasPendingSell) await conditionalSell(quantity, strikeUpper);
+
+    if (!borrowing && !aboveStrike) await immediateSell(quantity);
+
+    if (borrowing && aboveStrike && !holding) await immediateBuy(quantity * strikeLower);
+
+    if (borrowing && aboveStrike && holding && !hasPendingSell) await conditionalSell(quantity, strikeUpper);
+
+    if (borrowing && !aboveStrike && holding && !hasPendingSell) await immediateSell(quantity);
+
+    if (borrowing && !aboveStrike && !holding && !hasPendingBuy) await conditionalBuy(quantity * strikeLower, strikeLower);
+
+    inprocess = false;
+}
+
+try {
+    process.stdin.on('data', process.exit.bind(process, 0));
+
+    wsClient.on('update', data => {
+        console.log(`update: ${JSON.stringify(data, null, 2)}`);
+        (async () => { await InitializePosition(); })();
+    });
+
+    wsClient.subscribe(['ticketInfo', 'order'], true);
+
+    await InitializePosition();
+
+    while (true) {
+        await asyncSleep(1000);
+        // let rr = await client.repayCrossMarginLoan('ETH', `${quantity}`);
+
+    }
+}
+catch (err) {
+    console.error(err);
 }
