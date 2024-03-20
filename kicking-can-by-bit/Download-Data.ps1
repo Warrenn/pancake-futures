aabf68690e3c898d0d2be5de04e186d14875aaf8
Warrenn Enslin <warrennenslin@Warrenns-MacBook-Pro.local>
2024-03-20 12:11:45 +0200

    download data

---
+++ kicking-can-by-bit/Download-Data.ps1
@@ -0,0 +1,23 @@
+param (
+    [string]$folder = "~/Workbench/Data/",
+    [string]$startDate = "2017-08-17",
+    [string]$symbol = "ETHUSDT"
+)
+
+$folder += "${symbol}/"
+if(-not (Test-Path $folder)) {
+    New-Item -ItemType Directory -Force -Path $folder
+}
+$today = [DateTime]::Today
+$date = [DateTime]::ParseExact($startDate, "yyyy-MM-dd", $null)
+while ($true) {
+    # Download data from a url
+    $url = "https://data.binance.vision/data/spot/daily/klines/${symbol}/1m/${symbol}-1m-$($date.ToString("yyy-MM-dd")).zip"
+    $output = "$folder${symbol}-$($date.ToString("yyyy-MM-dd")).zip"
+    write-host "Downloading $url $output"
+    Invoke-WebRequest -Uri $url -OutFile $output
+    $date = $date.AddDays(1)
+    if($date -gt $today) {
+        break
+    }
+}
